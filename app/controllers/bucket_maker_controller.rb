4cc17ff88f33995189d47ee78582eb3d14dec940
Dinesh Vasudevan <dvasudevan@babbel.com>
2013-12-26 00:20:39 +0100

    Refactored the controller

--- app/controllers/bucket_maker_controller.rb
+++ app/controllers/bucket_maker_controller.rb
@@ -1,28 +1,32 @@
 class BucketMakerController < ApplicationController
   include BucketMakerConcern
 
+  before_filter :ensure_valid_series_bucket_group, only: [:show, :switch]
+  before_filter :ensure_valid_series_bucket, only: [:randomize]
+
   def show
-    if BucketMaker.buckets_configuration.has_group_in_bucket_in_series?(@series_name, @bucket_name, @group_name)
-      render text: @current_user.in_bucket?(@series_name, @bucket_name, @group_name)
-    else
-      head :not_found
-    end
+    render text: @current_user.in_bucket?(@series_name, @bucket_name, @group_name)
   end
 
   def switch
-    if BucketMaker.buckets_configuration.has_group_in_bucket_in_series?(@series_name, @bucket_name, @group_name)
-      render text: @current_user.force_to_bucket!(@series_name, @bucket_name, @group_name)
-    else
-      head :not_found
-    end
+    render text: @current_user.force_to_bucket!(@series_name, @bucket_name, @group_name)
   end
 
   def randomize
-    if BucketMaker.buckets_configuration.has_bucket_in_series?(@series_name, @bucket_name)
-      render text: @current_user.bucketize_for_series_and_bucket!(@series_name, @bucket_name)
-    else
-      head :not_found
-    end
+    render text: @current_user.bucketize_for_series_and_bucket!(@series_name, @bucket_name)
   end
 
+  private
+  def ensure_valid_series_bucket_group
+    not_found unless BucketMaker.buckets_configuration.has_group_in_bucket_in_series?(@series_name, @bucket_name, @group_name)
+  end
+
+  def ensure_valid_series_bucket
+    not_found unless BucketMaker.buckets_configuration.has_bucket_in_series?(@series_name, @bucket_name)
+  end
+
+  def not_found
+    head :not_found
+    false
+  end
 end
